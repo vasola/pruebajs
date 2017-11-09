e20f3e74f547ffae31e95df3a21d529f0b968e69
Valentina <valentina.sotolara@gmail.com>
2017-11-09 18:15:04 -0300

    edicion JS

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,13 +1,24 @@
 $(document).ready(function(){
-    $("#tweet").on('submit',function(event){ event.preventDefault();
-        $('.col-md-7').append('
-        '<h5>titulo twitter</h5>',
-        + '<span>@hola</span>',
-        + '<p>kjdbkjnsajkdbash</p>',
-        + '<a href=""><i class="fa fa-comment-o" aria-hidden="true"></i></a>',
-        + '<a href=""><i class="fa fa-retweet" aria-hidden="true"></i></a>',
-        + '<a href=""><i class="fa fa-heart-o" aria-hidden="true"></i></a>',
-        + '<a href=""><i class="fa fa-trash-o" aria-hidden="true"></i></a>',
-        + '<a href=""><i class="fa fa-envelope-o" aria-hidden="true"></i></a>')
+    $("#tweet").on('submit',function(event){ 
+        event.preventDefault();
+        $('#board').append(
+                    '<div class="row note">'
+                    +'<div class="col-md-12">'
+                    +'<img src="images/perfil.jpg" alt="perfil">'
+                    +'<h5>Valentina Soto</h5>'
+                    + '<span>@va_sola</span>'
+                    + '<p>' + $('#myform').val() + '</p>'
+                    + '<a href="javascript:void(0)"><i class="fa fa-comment-o" aria-hidden="true"></i></a>'
+                    + '<a href="javascript:void(0)"><i class="fa fa-retweet" aria-hidden="true"></i></a>'
+                    + '<a href="javascript:void(0)"><i class="fa fa-heart-o" aria-hidden="true"></i></a>'
+                    + '<a href="javascript:void(0)"><i class="fa fa-trash-o" aria-hidden="true"></i></a>'
+                    + '<a href="javascript:void(0)"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>'
+                    + '</div>'
+                    + '</div>')
         });
+    
+    $('#board').on('click','.fa-trash-o', function(event){
+        event.preventDefault()
+        $(this).parent().parent().fadeOut(500);
+        })
 })
